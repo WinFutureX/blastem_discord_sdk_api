eb603257dac0e809377b9fdd1f5dd1ed8276f716
Kelsey Boey <kelseyboey@hotmail.com>
2020-01-11 10:36:41 +0800

    Make sure DiscordCreate uses fixed args

--- discord_all.c
+++ discord_all.c
@@ -43,8 +43,8 @@ int init_discord()
 	// attempt to load SDK API library
 	#ifdef _WIN32
 	// for windows
-	typedef int (__stdcall *f_crdiscord)();
 	void *discord_handle = LoadLibrary(DISCORD_LIB);
+	typedef int (__stdcall *f_crdiscord)(DiscordVersion version, struct DiscordCreateParams* params, struct IDiscordCore** result);
 	f_crdiscord create_discord = (f_crdiscord)GetProcAddress(discord_handle, "DiscordCreate");
 	#else
 	// for linux and possibly macos
